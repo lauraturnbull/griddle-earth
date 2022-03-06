4f17a7855b33c8a1c2ffe5f46ce8d50687ad71d2
lauraturnbull <lauraturnbull@bulb.co.uk>
2022-03-06 22:33:02 +0000

    Adds some items to play with.

--- engine/engine/core/resources/items.py
+++ engine/engine/core/resources/items.py
@@ -1,37 +1,77 @@
 from engine.core.types import ItemCollectionMethod, ItemType, NewItem
 
-# fruit
+# forage
 apple = NewItem(
-    name="apple",
+    name="apples",
     item_type=ItemType.fruit,
     health_points=30,
     collection_method=ItemCollectionMethod.forage,
 )
-
-# vegetables
-potato = NewItem(
-    name="potato",
+bamboo = NewItem(
+    name="bamboo",
     item_type=ItemType.vegetable,
     health_points=50,
     collection_method=ItemCollectionMethod.forage,
 )
 carrot = NewItem(
-    name="carrot",
+    name="carrots",
     item_type=ItemType.vegetable,
     health_points=50,
     collection_method=ItemCollectionMethod.forage,
 )
-
+chives = NewItem(
+    name="chives",
+    item_type=ItemType.herb,
+    health_points=5,
+    collection_method=ItemCollectionMethod.forage,
+)
+garlic = NewItem(
+    name="garlic",
+    item_type=ItemType.vegetable,
+    health_points=50,
+    collection_method=ItemCollectionMethod.forage,
+)
+pear = NewItem(
+    name="pears",
+    item_type=ItemType.vegetable,
+    health_points=50,
+    collection_method=ItemCollectionMethod.forage,
+)
+potato = NewItem(
+    name="potatoes",
+    item_type=ItemType.vegetable,
+    health_points=50,
+    collection_method=ItemCollectionMethod.forage,
+)
+pumpkin = NewItem(
+    name="pumpkins",
+    item_type=ItemType.vegetable,
+    health_points=50,
+    collection_method=ItemCollectionMethod.forage,
+)
+rhubarb = NewItem(
+    name="rhubarb",
+    item_type=ItemType.fruit,
+    health_points=40,
+    collection_method=ItemCollectionMethod.forage,
+)
 spotted_mushroom = NewItem(
-    name="spotted mushroom",
+    name="spotted mushrooms",
     item_type=ItemType.vegetable,
     health_points=-30,
     collection_method=ItemCollectionMethod.forage,
 )
+strawberry = NewItem(
+    name="strawberries",
+    item_type=ItemType.fruit,
+    health_points=20,
+    collection_method=ItemCollectionMethod.forage,
+)
 
-# proteins
+
+# hunt
 egg = NewItem(
-    name="egg",
+    name="eggs",
     item_type=ItemType.protein,
     health_points=40,
     collection_method=ItemCollectionMethod.forage,
@@ -54,14 +94,5 @@ wheat = NewItem(
     name="wheat",
     item_type=ItemType.grain,
     health_points=50,
-    collection_method=ItemCollectionMethod.forage,
-)
-
-# herbs
-
-chives = NewItem(
-    name="chives",
-    item_type=ItemType.herb,
-    health_points=5,
     collection_method=ItemCollectionMethod.forage,
 )
